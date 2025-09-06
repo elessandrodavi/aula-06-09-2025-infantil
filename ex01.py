14ffbb7d7ddfb8719ee5edadf76fb44fe3cb0e08
elessandrodavi <elessandrodavi@gmail.com>
2025-09-06 18:42:47 -0300

    up

---
+++ ex01.py
@@ -0,0 +1,15 @@
+#
+# autor: davi 
+# data: 06/09/2025
+while True:
+# entrada de dados
+    idade = int(input("informe sua idade: "))
+
+
+    # processamento
+    if (idade >= 18):
+        print("você e maior de idade!")
+    else:
+        print("você é menor de idade!") 
+        print("feche o aplicativo!")
+        break        
